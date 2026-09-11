c27d64665474e32de6af4539ec0358ecfa7f6138
Abhinav Sahu <abhisah18.lko@gmail.com>
2026-09-11 10:12:51 +0530

    Added ArrayList Examples

--- src/arrayList/list1.java
+++ src/arrayList/list1.java
@@ -1,7 +1,6 @@
 package arrayList;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class list1 {
     public static void main(String[] args) {
@@ -9,17 +8,7 @@ public class list1 {
         ArrayList<Integer> list=new ArrayList<>();
         list.add(1);
         list.add(213);
-        list.add(2);
-        for(int i:list)
-            System.out.println(i);
-//        Scanner sc=new Scanner(System.in);
-//        int a[]=new int[10];
-//        int sum=0;
-//        for(int i=0;i<a.length;i++)
-//            a[i]=sc.nextInt();
-//        for(int i=0;i<a.length;i++)
-//        {
-//            sum+=a[i];
-//        }
+        list.addFirst(2);
+        System.out.println(list);
     }
 }
